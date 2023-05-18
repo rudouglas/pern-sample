f941819571e5d057c9362f3bcdaaebc4db50d046
rdouglas <ruairidouglas@gmail.com>
2023-05-18 14:27:53 +0100

    Update http-common.js

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pern-example.onrender.com/api",
+  baseURL: "https://pern-example.onrender.com/api/tutorials",
   headers: {
     "Content-type": "application/json"
   }
