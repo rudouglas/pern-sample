ccc1368921bc6abb688dfc3ff58a031b0f1ff170
rdouglas <ruairidouglas@gmail.com>
2023-05-18 15:01:13 +0100

    Update http-common.js

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const PORT = process.env.PORT || 8080;
 
 export default axios.create({
-  baseURL: "https://pern-example.onrender.com/api/tutorials",
+  baseURL: `https://localhost:${PORT}/api/tutorials`,
   headers: {
     "Content-type": "application/json"
   }
