04369e1cfae1b2e627f7990c25af6517b4d7c052
rdouglas <ruairidouglas@gmail.com>
2023-05-19 09:54:00 +0100

    fix: issue with host

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const PORT = process.env.PORT || 8080;
 
 export default axios.create({
-  baseURL: `https://0.0.0.0:${PORT}/api/tutorials`,
+  baseURL: `https://0.0.0.0:${PORT}/api`,
   headers: {
     "Content-type": "application/json"
   }
